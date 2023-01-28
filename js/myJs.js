ed18a0cc70424722fe8bbd31c7cbdb4b01550f6c
Mehdi-Khashi <mehdi.k.0711@gmail.com>
2023-01-28 20:39:00 +0330

    v6.2
    v6.2

--- js/myJs.js
+++ js/myJs.js
@@ -4,26 +4,41 @@ function myFunction(e) {
     var coor = "Coordinates: (" + x + "," + y + ")";
     // console.log(coor);
     // document.getElementById("movethis").style.top = document.getElementById("movethis").getBoundingClientRect().top - 15 + 'px';
-    if(y < 400){
-        document.getElementById("movethis").style.top = 24 +'%';
-    }else if(y < 560 ){
-        document.getElementById("movethis").style.top = 30 +'%';
-    }else if(y > 570 ){
-        document.getElementById("movethis").style.top = 34 +'%';
+    if (y < 400) {
+        document.getElementById("movethis").style.top = 24 + '%';
+    } else if (y < 560) {
+        document.getElementById("movethis").style.top = 30 + '%';
+    } else if (y > 570) {
+        document.getElementById("movethis").style.top = 34 + '%';
     }
 
 
-    if(x < 660 ){
-        document.getElementById("movethis").style.left = 30 +'%';
-    }else if(x < 900){
-        document.getElementById("movethis").style.left = 34 +'%';
-    }else if(x > 900){
-        document.getElementById("movethis").style.left = 38 +'%';
+    if (x < 660) {
+        document.getElementById("movethis").style.left = 30 + '%';
+    } else if (x < 900) {
+        document.getElementById("movethis").style.left = 34 + '%';
+    } else if (x > 900) {
+        document.getElementById("movethis").style.left = 38 + '%';
     }
-   
-  }
-  
-  function clearCoor() {
-    document.getElementById("movethis").style.top = 30 +'%';
-    document.getElementById("movethis").style.left = 34 +'%';
-  }
+
+}
+
+function clearCoor() {
+    document.getElementById("movethis").style.top = 30 + '%';
+    document.getElementById("movethis").style.left = 34 + '%';
+}
+
+
+function blackScreen() {
+    document.getElementById('blackSc').style.cssText = 'width: 100%;';
+    setTimeout(()=> {
+        document.getElementById('hideAll').style.cssText = 'width: 0%;';
+     }
+     ,900);
+    
+}
+
+function blackScreenHide() {
+    document.getElementById('blackSc').style.cssText = 'width: 0%;';
+    document.getElementById('hideAll').style.cssText = 'width: 100%;';
+}
